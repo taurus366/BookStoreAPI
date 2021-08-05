4d77db604731945c6a87e83ec536160c213fd347
taurus366 <47596673+taurus366@users.noreply.github.com>
2021-08-05 12:26:06 +0300

    Initial commit

---
+++ src/main/java/com/example/bookstore/data/config/GsonConfig.java
@@ -0,0 +1,18 @@
+package com.example.bookstore.data.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GsonConfig {
+
+    @Bean
+    public Gson gson(){
+        return new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .setPrettyPrinting()
+                .create();
+    }
+}
