5b92b254e2037c93f8197b117fc533cfef8579b2
taurus366 <47596673+taurus366@users.noreply.github.com>
2021-08-28 00:37:43 +0300

    added admin order accept function finished!

--- src/main/java/com/example/bookstore/data/init/Engine.java
+++ src/main/java/com/example/bookstore/data/init/Engine.java
@@ -1,7 +1,8 @@
 package com.example.bookstore.data.init;
 
-import com.example.bookstore.data.entities.Book;
+import com.example.bookstore.data.entities.*;
 import com.example.bookstore.data.service.BookService;
+import com.example.bookstore.data.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,23 +15,36 @@ public class Engine implements CommandLineRunner {
     @Autowired
     private final BookService bookService;
 
-    public Engine(BookService bookService) {
+    @Autowired
+    private final UserService userService;
+
+    public Engine(BookService bookService, UserService userService) {
         this.bookService = bookService;
+        this.userService = userService;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
 
-        Book book = new Book("ali' Book", "ali zinal", 2.54, new Timestamp(System.currentTimeMillis()), "ali's home", 255, "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Book_stub_img.svg/1024px-Book_stub_img.svg.png");
-        Book book1 = new Book("ali' Book", "ali zinal", 2.54, new Timestamp(System.currentTimeMillis()), "ali's home", 255, "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Book_stub_img.svg/1024px-Book_stub_img.svg.png");
-        Book book2= new Book("ali' Book", "ali zinal", 2.54, new Timestamp(System.currentTimeMillis()), "ali's home", 255, "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Book_stub_img.svg/1024px-Book_stub_img.svg.png");
-        Book book3 = new Book("ali' Book", "ali zinal", 2.54, new Timestamp(System.currentTimeMillis()), "ali's home", 255, "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Book_stub_img.svg/1024px-Book_stub_img.svg.png");
+        Book book = new Book("The New York Cafe", "Michael Dean", 22.54, new Timestamp(System.currentTimeMillis()), "English's home", 257, "https://english-e-reader.net/covers/The_New_York_Cafe-Michael_Dean.jpg");
+        Book book1 = new Book("Blue Moon Beach", "Sue Murray", 25.55, new Timestamp(System.currentTimeMillis()), "Murray's home", 255, "https://english-e-reader.net/covers/Blue_Moon_Beach-Sue_Murray.jpg");
+        Book book2= new Book("Future Energy", "Alex Raynham", 35.54, new Timestamp(System.currentTimeMillis()), "Alex's home", 370, "https://english-e-reader.net/covers/Future_Energy-Alex_Raynham.jpg");
+        Book book3 = new Book("Catch Me If You Can", "Frank W. Abagnale", 57.54, new Timestamp(System.currentTimeMillis()), "Alex's home", 300, "https://english-e-reader.net/covers/Catch_Me_If_You_Can-Frank_W_Abagnale.jpg");
 
         this.bookService.postBook(book);
         this.bookService.postBook(book1);
         this.bookService.postBook(book2);
         this.bookService.postBook(book3);
+
+
+//   ADMIN PROFILE
+        User user = new User("admin", "admin", "adminadmin@gmail.com", "12341234", "0894396766", Gender.Male,"street seventeen number nine");
+        user.setRole(Role.ADMIN);
+        Session session = new Session("saduasuidhywi535i3ohjfkl", new Timestamp(System.currentTimeMillis()), user);
+        user.setSession(session);
+
+        userService.postUser(user);
 
     }
 }
